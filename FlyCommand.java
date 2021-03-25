82e68e9a853cd83e21165a0f77d06ebbcf0a67b4
stats <75351729+stats0@users.noreply.github.com>
2021-03-25 17:14:27 -0500

    Update FlyCommand.java

--- FlyCommand.java
+++ FlyCommand.java
@@ -1,7 +1,5 @@
 package me.stats0.EssentialPlugin.commands;
 
-import java.util.ArrayList;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,35 +9,38 @@ import me.stats0.EssentialPlugin.Main;
 
 public class FlyCommand implements CommandExecutor {
 	
-	private ArrayList<Player> list_of_flying_players = new ArrayList<>();
-	
 	private Main plugin;
 	
 	public FlyCommand(Main plugin) {
 		this.plugin = plugin;
-		plugin.getCommand("/fly").setExecutor(this);
+		
+		plugin.getCommand("fly").setExecutor(this);
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
 		if (!(sender instanceof Player)) {
-			sender.sendMessage("Only players may execute this command");
+			sender.sendMessage(Utils.chat(plugin.getConfig().getString("console_error_message")));
 			return true;
 		}
 		
 		Player p = (Player) sender;
 		
-		if (player.hasPermission("EP.fly")) {
-			if (list_of_flying_players.contains(player)){
-				list_of_flying_players.remove(player);
+		if (p.hasPermission("ep.fly")) {
+			
+			if (p.isFlying()) {
 				p.setAllowFlight(false);
-				p.sendMessage("You have disabled flight");
-			} else if(!list_of_flying_players.contains(player)) {
-			list_of_flying_players.add(player);
-			p.setAllowFlight(true);
-			p.sendMessage("You can now fly.");
+				p.setFlying(false);
+				p.sendMessage(Utils.chat(plugin.getConfig().getString("FlyCommand.flying_disabled")));
+				return true;
+			} else {
+				p.setAllowFlight(true);
+				p.setFlying(true);
+				p.sendMessage(Utils.chat(plugin.getConfig().getString("FlyCommand.flying_enabled")));
+			}
 		} else {
-			p.sendMessage("You do not have permission to use this command.");
+			p.sendMessage(Utils.chat(plugin.getConfig().getString("no_perm_message")));
 		}
 		
 		return false;
