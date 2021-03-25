5629aba69c1e21b4baf3382f79b82f7f7e1bc8fe
stats <75351729+stats0@users.noreply.github.com>
2021-03-25 17:16:03 -0500

    Create utils.java

---
+++ utils.java
@@ -0,0 +1,12 @@
+package me.stats0.EssentialPlugin.utils;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class utils {
+	
+	public static String chat (String s) {
+		return ChatColor.translateAlternateColorCodes('&', s);
+	}
+	
+	
+}
